01e88fdbf170d1f6a64a831316ecd738d1dbd875
krawthekrow <krawthekrow@gmail.com>
2021-07-21 00:32:12 -0400

    hud: make hud more compact

--- src/simulation/ElementClasses.h
+++ src/simulation/ElementClasses.h
@@ -9,3 +9,6 @@
 #undef ELEMENT_NUMBERS_ENUMERATE
 
 std::array<Element, PT_NUM> const &GetElements();
+
+const String FILT_MODES[] = {"SET", "AND", "OR", "SUB", "<<", ">>", "no eff", "XOR", "NOT", "QRTZ scat", "<<<", ">>>"};
+const int FILT_NUM_MODES = sizeof(FILT_MODES) / sizeof(FILT_MODES[0]);
